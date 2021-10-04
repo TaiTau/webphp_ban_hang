6f85e4865c7572a1609c8b944f70b5e7b8bda2e0
long duong <longduong801@gmail.com>
2021-10-04 18:13:03 +0700

    long

--- include/chitietsp.php
+++ include/chitietsp.php
@@ -5,7 +5,7 @@
 	$sql_category = mysqli_query($con,'SELECT * FROM tbl_category ORDER BY category_id DESC');
 
 ?>
-<div class="small-container single-product">
+<div class="product-details">
     <?php
         if(isset($_GET['id'])){
             $id = $_GET['id'];
@@ -17,13 +17,10 @@
     <?php
         while($row_chitiet = mysqli_fetch_array($sql_chitiet)){
     ?>
-    <div class="row">
-        <div class="col-2">
-            <img src="uploads/<?php echo $row_chitiet['sanpham_image'] ?>" data-imagezoom="true" alt="">
-        </div>
-        <div class="col-2">
-            <p><?php echo $row_chitiet['sanpham_name'] ?></p>
-            <h4><?php echo number_format($row_chitiet['sanpham_giakhuyenmai']).'₫'?></h4>
+    <div class="wrapper">
+      <div class="right">
+        <div class="logo" style="color: #8d8d8d;"><?php echo $row_chitiet['sanpham_name'] ?></div>
+        <h1 style="color: black;"><?php echo number_format($row_chitiet['sanpham_giakhuyenmai']).'₫'?></h1>
             <form action="?quanly=giohang" method="post">
                 <input type="hidden" name="tensanpham" value="<?php echo $row_chitiet['sanpham_name'] ?>">
                 <input type="hidden" name="sanpham_id" value="<?php echo $row_chitiet['sanpham_id'] ?>">
@@ -32,11 +29,24 @@
                 <input type="hidden" name="soluong" value="1">
                 <input type="submit" name="themgiohang" value="Thêm vào giỏ hàng" class="btn"></input>
             </form>
-            <h3>Thông số kĩ thuật</h3>
-            <p><?php echo $row_chitiet['sanpham_mota'] ?></p>
         </div>
+      <div class="left">
+        <img src="uploads/<?php echo $row_chitiet['sanpham_image'] ?>" data-imagezoom="true" alt="">
+      </div>
+    </div>
+    <div class="wrapper">
+      <div class="bottom">
+        <ul>
+          <li>Chipset Apple A14 Bionic (5 nm)</li>
+          <li>CPU	Hexa-core</li>
+          <li>GPU	Apple GPU (4-core graphics)</li>
+          <li>Dual	12 MP, f/2.2, 23mm (wide), 1/3.6"</li>
+          <li>Loudspeaker	Yes, with stereo speakers</li>
+          <li>USB	Lightning, USB 2.0</li>
+        </ul>
+      </div>
     </div>
     <?php
 	}
     ?>
-</div>
+  </div>
